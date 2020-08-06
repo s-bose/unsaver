a3eafadf093595d3fcfc3eaf2df72d0af891d4fe
s-bose <shiladitya_basu@live.com>
2020-08-06 17:56:09 +0530

    --started login page design

--- unsaver/my_site/urls.py
+++ unsaver/my_site/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.default_view, name='default'),
     path('home', views.home_view, name="home"),
     path('login', views.login_view, name="login"),
-    path('authenticated', views.authenticated, name="authenticated")
+    path('auth_callback', views.auth_callback, name="auth_callback")
 ]
--- unsaver/my_site/views.py
+++ unsaver/my_site/views.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 import praw
 import random
 
-
+reddit = praw.Reddit("unsaver", user_agent="unsaver for reddit")
 # Create your views here.
 def default_view(request):
     # request.session['refresh_token'] = 'test'
@@ -23,8 +23,9 @@ def home_view(request):
         print(f'code is {code}\nrefresh_token is {rf}')
         reddit_authenticated = praw.Reddit("unsaver", refresh_token=rf, user_agent="unsaver for reddit")
         user_name = reddit_authenticated.user.me()
-        for saved_posts in reddit_authenticated.user.me().saved(limit=5, params={"after": "t3_i0ut1t"}):
-            print(saved_posts.id)
+        for saved_posts in reddit_authenticated.user.me().saved(limit=5):
+            # print(isinstance(saved_posts, praw.models.Comment))
+            print(saved_posts.over_18)
         return HttpResponse(user_name)
     else:
         return redirect('login')
@@ -33,14 +34,12 @@ def home_view(request):
 def login_view(request):
     if (request.session.get('refresh_token')):
         return redirect('home')
-    reddit = praw.Reddit("unsaver", user_agent="unsaver for reddit")
     authentication_uri = reddit.auth.url(["identity", "read", "save", "history"], random.randint(1, 99999), "permanent")
-    # template = loader.get_template('my_site/login.html')
-    # return HttpResponse(template.render({'auth_url': authentication_uri}, request))  
-    return JsonResponse({"auth_uri": authentication_uri})
+    template = loader.get_template('my_site/login.html')
+    return HttpResponse(template.render({'auth_url': authentication_uri}, request))  
+    # return JsonResponse({"auth_uri": authentication_uri})
     
-def authenticated(request):
-    reddit = praw.Reddit("unsaver", user_agent="unsaver for reddit")
+def auth_callback(request):
     code = request.GET.get('code', '')
     refresh_token = reddit.auth.authorize(code)
     request.session['code'] = code
