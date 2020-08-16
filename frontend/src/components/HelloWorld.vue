d3c173392d74d45f990d0318034798ea2ad28741
Shiladitya Bose <shiladitya.31.z@gmail.com>
2020-08-16 13:36:59 +0530

    pure html too bad, using vue MPA now to bundle login and home pages

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,15 +1,61 @@
+<template>
+  <div class="container-fluid">
+      <div class="row no-gutter">
+        <div class="d-none d-md-flex justify-content-center col-md-6 col-lg-6 bg-image">
+          <div class="card2" style="top: 29vh;"></div>
+          <div class="card2">
+            <div class="image-skeleton"></div>
+          </div>
+          <div class="card">
+            <div class="image-skeleton"></div>
+            <div class="avatar-skeleton"></div>
+            <div class="text-skeleton"></div>
+            <div class="text-skeleton" style="width: 85%"></div>
+            <div class="text-skeleton" style="height: 3%; width: 70%; border-radius: 10em; top: 3em"></div>
+          </div>  
+        </div>
+        
+        <div class="col-md-6 col-lg-6">
+          <div class="login d-flex align-items-center py-5">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-9 col-lg-8 mx-auto">
+                  <div class="jumbotron">
+                      <h1 class="display-4">Unsaver</h1>
+                      <p class="lead">Clear your Reddit saved posts with a swipe</p>
+                      <hr class="my-4">
+                      <p>Unsaver for Reddit uses Tinder-like swipe cards to help you browse through your saved posts easily</p>
+                      <div class="btn-container">
+                          <a class="btn btn-secondary btn-lg btn-white btn-block" href="" role="button">
+                            <i class="fab fa-reddit fa-2x fa-fw" style="vertical-align: middle;"></i>
+                              <span>Login to Reddit</span></a>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
 
-  :root {
-      --input-padding-x: 1.5rem;
-      --input-padding-y: 0.75rem;
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
   }
+}
+</script>
 
-  body {
-    font-family: 'Quicksand';
-  }
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
   
   .bg-image {
-    background-image: url('/static/login/reddit_snoo.jpg');
+    background-image: url('../assets/reddit_snoo.jpg');
     background-size: cover;
     background-position: 20%;
     position: relative; 
@@ -163,3 +209,5 @@
   i {
     margin-right: 0.2em;
   }
+
+</style>
